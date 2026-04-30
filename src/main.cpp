808b0e2dc7c9da9bc53780e5bb961e6dd111ad46
Justin White <jw@justinwhite.net>
2026-04-30 14:40:55 -0400

    implement basic "drain" mode

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #define SMALL_MODE
 
 // makes seconds strip complete in a single second, for testing
-#define FAST_MODE
+// #define FAST_MODE
 
 #ifdef FULL_MODE
 constexpr uint MINUTES_STRIP_COUNT = 60;
@@ -58,11 +58,18 @@ int prevSecPos;
 int minPos;
 int prevMinPos;
 
+int secRemain = SECONDS_STRIP_COUNT;
+int minRemain = MINUTES_STRIP_COUNT / 2;
+
+bool alertActive = false;
+int alertCount = 10;
+
 enum MODES {
   DROP,
-  FILL,
-  EMPTY
-} mode = FILL;
+  // FILL,
+  DRAIN,
+  ALERT
+} mode = DRAIN;
 
 uint hueBase = 0;
 
@@ -83,20 +90,31 @@ void setup() {
   secStrip.fill();
   secStrip.show();
 
-  #ifdef SECONDS_FORWARD
-  secPos = -1;
-  prevSecPos = secStrip.numPixels() - 1;
-  #else
-  secPos = secStrip.numPixels() - 1;
-  prevSecPos = 0;
-  #endif
-  #ifdef MINUTES_FORWARD
-  minPos = 0;
-  prevMinPos = minStrip.numPixels() - 1;
-  #else
-  minPos = minStrip.numPixels() - 1;
-  prevMinPos = 0;
-  #endif
+  if (mode == DRAIN) {
+    secStrip.clear();
+    minStrip.clear();
+    secStrip.fill(0xFFFFFF, 0, secRemain);
+    minStrip.fill(0xFFFFFF, 0, minRemain);
+    secStrip.show();
+    minStrip.show();
+  }
+
+  if (mode == DROP) {
+    #ifdef SECONDS_FORWARD
+    secPos = -1;
+    prevSecPos = secStrip.numPixels() - 1;
+    #else
+    secPos = secStrip.numPixels() - 1;
+    prevSecPos = 0;
+    #endif
+    #ifdef MINUTES_FORWARD
+    minPos = 0;
+    prevMinPos = minStrip.numPixels() - 1;
+    #else
+    minPos = minStrip.numPixels() - 1;
+    prevMinPos = 0;
+    #endif
+  }
 }
 
 void loop() {
@@ -104,54 +122,95 @@ void loop() {
 
   if (now - lastUpdate > WAIT_TIME) {
     lastUpdate = now;
-    prevSecPos = secPos;
 
-    #ifdef SECONDS_FORWARD
-    secPos += 1;
-    if (secPos >= secStrip.numPixels()) {
-      secPos = 0;
-      secStrip.clear();
-      updateMinutes = true;
-    }
-    #else
-    secPos -= 1;
-    if (secPos < 0) {
-      secPos = secStrip.numPixels() - 1;
-      secStrip.clear();
-      updateMinutes = true;
-    }
-    #endif
+    if (mode == ALERT) {
+      if (alertCount <= 0) {
+        secStrip.fill(0xFF0000);
+        minStrip.fill(0xFF0000);
+        secStrip.show();
+        minStrip.show();
+        delay(100);
+        return;
+      }
+      alertCount--;
 
-    if (updateMinutes) {
-      updateMinutes = false;
-      prevMinPos = minPos;
-
-      #ifdef MINUTES_FORWARD
-      minPos += 1;
-      if (minPos >= minStrip.numPixels()) {
-        minPos = 0;
+      if (alertActive) {
+        secStrip.fill(0xFF0000);
+        minStrip.fill(0xFF0000);
+      } else {
+        secStrip.clear();
         minStrip.clear();
       }
-      #else
-      minPos -= 1;
-      if (minPos < 0) {
-        minPos = minStrip.numPixels() - 1;
-        minStrip.clear();
-      }
-      #endif
+      secStrip.show();
+      minStrip.show();
+      alertActive = !alertActive;
     }
 
-    hueBase += 256;
-    const uint secHue = hueBase + secPos * 65536 / minStrip.numPixels();
-    const uint color = Adafruit_NeoPixel::gamma32(
-      Adafruit_NeoPixel::ColorHSV(secHue)
-    );
-    secStrip.setPixelColor(secPos, color);
-    secStrip.show();
-    minStrip.setPixelColor(minPos, color);
-    minStrip.show();
+    if (mode == DRAIN) {
+      secRemain--;
+      secStrip.clear();
+      minStrip.clear();
+      secStrip.fill(0xFFFFFF, 0, secRemain);
+      minStrip.fill(0xFFFFFF, 0, minRemain);
+      secStrip.show();
+      minStrip.show();
+      if (secRemain <= 0) {
+        secRemain = SECONDS_STRIP_COUNT;
+        minRemain--;
+      }
+      if (minRemain <= 0) {
+        mode = ALERT;
+      }
+    }
 
     if (mode == DROP) {
+      prevSecPos = secPos;
+
+      #ifdef SECONDS_FORWARD
+      secPos += 1;
+      if (secPos >= secStrip.numPixels()) {
+        secPos = 0;
+        secStrip.clear();
+        updateMinutes = true;
+      }
+      #else
+      secPos -= 1;
+      if (secPos < 0) {
+        secPos = secStrip.numPixels() - 1;
+        secStrip.clear();
+        updateMinutes = true;
+      }
+      #endif
+
+      if (updateMinutes) {
+        updateMinutes = false;
+        prevMinPos = minPos;
+
+        #ifdef MINUTES_FORWARD
+        minPos += 1;
+        if (minPos >= minStrip.numPixels()) {
+          minPos = 0;
+          minStrip.clear();
+        }
+        #else
+        minPos -= 1;
+        if (minPos < 0) {
+          minPos = minStrip.numPixels() - 1;
+          minStrip.clear();
+        }
+        #endif
+      }
+
+      hueBase += 256;
+      const uint secHue = hueBase + secPos * 65536 / minStrip.numPixels();
+      const uint color = Adafruit_NeoPixel::gamma32(
+        Adafruit_NeoPixel::ColorHSV(secHue)
+      );
+      secStrip.setPixelColor(secPos, color);
+      secStrip.show();
+      minStrip.setPixelColor(minPos, color);
+      minStrip.show();
+
       delay(switchTime);
       secStrip.setPixelColor(prevSecPos, 0);
       secStrip.show();
